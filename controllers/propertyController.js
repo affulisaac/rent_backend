a3c682b0ddf688596a347e0ce451801274afe61e
Isaac Afful <isaac.afful@hubtel.com>
2023-05-12 20:15:23 +0000

    updated Property controller

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -1,35 +1,58 @@
 const asyncHandler = require("express-async-handler");
-const Property = require("../model/propertyModel")
+const Property = require("../model/propertyModel");
 
-const getProperties = asyncHandler(async (req, res) => {
-  const property = await Property.find()
+const getProperties = async (req, res) => {
+  try {
+      const property = await Property.find();
   res.status(200).json(property);
-});
+  } catch (error) {
+    res.status(400).json({ message: error?.message });
 
-const addProperty = asyncHandler(async (req, res) => {
-  const property = await Property.create(req.body);
-  res.status(200).json(property);
-});
-
-const updateProperty = asyncHandler(async (req, res) => {
-  const property = await Property.findById(req.params.id);
-  if (!property) {
-    res.status(400);
-    throw new Error("Property not found");
   }
-  const updatedProperty = Property.findByIdAndUpdate(req.params.id, req.body).populate('user');
-  res.status(200).json(updatedProperty._update);
-});
 
-const deleteProperty = asyncHandler(async (req, res) => {
-  const property = await Property.findById(req.params.id);
-  if (!property) {
-    res.status(400);
-    throw new Error(`Could not find property with ID ${req.params.id}`);
+};
+
+const addProperty = async (req, res) => {
+  try {
+    const property = await Property.create(req.body);
+    res.status(200).json(property);
+  } catch (error) {
+    res.status(400).json({ message: error?.message });
   }
-  await Property.findByIdAndDelete(req.params.id);
-  res.status(200).json({ id: req.params.id, message: "Deleted successfully" });
-});
+};
+
+const updateProperty = async (req, res) => {
+  try {
+    const property = await Property.findById(req.params.id);
+    if (!property) {
+      res.status(400);
+      throw new Error("Property not found");
+    }
+    const updatedProperty = Property.findByIdAndUpdate(
+      req.params.id,
+      req.body
+    ).populate("user");
+    res.status(200).json(updatedProperty._update);
+  } catch (error) {
+    res.status(400).json({ message: error?.message });
+  }
+};
+
+const deleteProperty = async (req, res) => {
+  try {
+    const property = await Property.findById(req.params.id);
+    if (!property) {
+      res.status(400);
+      throw new Error(`Could not find property with ID ${req.params.id}`);
+    }
+    await Property.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ id: req.params.id, message: "Deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error?.message });
+  }
+};
 
 const getProperty = asyncHandler(async (req, res) => {
   const property = await Property.findById(req.params.id).catch((err) => {
@@ -40,5 +63,9 @@ const getProperty = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  getProperties, addProperty, deleteProperty, updateProperty, getProperty
+  getProperties,
+  addProperty,
+  deleteProperty,
+  updateProperty,
+  getProperty,
 };
