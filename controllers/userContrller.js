f0edc95cb5b019d58b62af39cf07c949551ee933
isaac.afful@hubtel.com <isaac.afful@hubtel.com>
2024-11-26 16:58:50 +0000

    Remove user authentication logic from loginUser function

--- controllers/userContrller.js
+++ controllers/userContrller.js
@@ -27,19 +27,6 @@ const loginUser = asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(400).json({ message: error?.message });
   }
-
-  const user = await User.findOne({ email });
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    });
-  } else {
-    res.status(400);
-    throw new Error("Email or password is not correct");
-  }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
